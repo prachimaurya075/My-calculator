d6210dd08f905196079aa92803cb03055ebd6d11
prachimaurya075 <146657736+prachimaurya075@users.noreply.github.com>
2024-02-04 13:27:52 +0530

    Add files via upload
    I have made a calculator using HTML CSS Javascript...

---
+++ Project Making calculator/script.js
@@ -0,0 +1,34 @@
+ let string = "";
+ let buttons = document.querySelectorAll('.button', );
+// // var buttons = document.querySelectorAll('.button-num', );
+// // var buttons = document.querySelectorAll('.button-n', );
+
+
+
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target. innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value =string;
+    }
+    else if(e.target. innerHTML == '+/='){
+      string = eval(string);
+      document.querySelector('input').value =string;
+    }
+     else if(e.target. innerHTML == 'AC'){
+      string = "";
+      document.querySelector('input').value =string;
+    }
+    else if(e.target. innerHTML == 'Del'){
+      string = "";
+      document.querySelector('input').value =string;
+    }
+    else{
+  console.log(e.target)
+  string = string +e.target. innerHTML;
+  document.querySelector('input').value = string;
+
+    }
+})
+
+})
